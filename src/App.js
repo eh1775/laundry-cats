b8db8c7cda2448b1bfab22d0d564a356e8d79bfa
Emily <eh1775@nyu.edu>
2019-05-02 15:46:54 +0800

    added logo

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import logo from './logo.png';
 import './App.css';
 
 class App extends Component{
@@ -49,7 +50,7 @@ class App extends Component{
   render(){
     return (
       <div className = "App">
-      <h1>Laundry Cats Work Order Form</h1>
+      <img src={logo}/>
       <form onSubmit>
       <label>
         Name: 
