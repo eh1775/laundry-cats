49c4d40bfea3ea72561d5fad69d0d5ca1612d6ae
SelinaXie <selinaxiem@gmail.com>
2019-05-08 10:28:15 +0800

    Add firebase config

---
+++ src/firebase_config.js
@@ -0,0 +1,10 @@
+export const firebase_config = {
+    //our firebase connection information from firebase.google.com
+    apiKey: "AIzaSyC0lXR6uza75WLNnF0CT0qB2Wx3aXfKjnc",
+    authDomain: "lab12-e1455.firebaseapp.com",
+    databaseURL: "https://lab12-e1455.firebaseio.com",
+    projectId: "lab12-e1455",
+    storageBucket: "lab12-e1455.appspot.com",
+    messagingSenderId: "42471833846",
+    appId: "1:42471833846:web:5eee959d0ef477e0"
+};
